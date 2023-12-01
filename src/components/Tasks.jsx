22295ae3f64eac7d5b8f77b1833a579a2219c519
Meani Freitas <meani.sf@gmail.com>
2023-12-01 16:55:55 -0300

    Created and done tasks counter

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,12 @@
 import Task from './Task';
 import styles from './Tasks.module.css';
 
-const Tasks = ({tasks}) => {
+const Tasks = ({createdCount = 5, doneCount='2/5'}) => {
   return (
     <section className={styles.container}>
       <div className={styles.links}>
-        <a href="#">Created tasks</a>
-        <a href="#">Done</a>
+        <a href="#" className={styles.created}>Created tasks <span className={styles.createdCount}>{createdCount}</span></a>
+        <a href="#" className={styles.done}>Done <span className={styles.doneCount}>{doneCount}</span></a>
       </div>
       <div className={styles.tasks}>
         <Task />
